90a7657a2c4b9900c537af1ef4d1152cd9a7f6ac
mysteo91 <mysteo91work@rambler.ru>
2021-08-10 12:55:10 +0300

    --test

---
+++ Core/Inc/display.h
@@ -0,0 +1,17 @@
+//
+// Created by arbin on 29.06.2021.
+//
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+#include "stm32f1xx_hal.h"
+#include "font.h"
+
+#define RESET_DISPLAY_PIN GPIO_PIN_1
+#define RESET_DISPLAY_PORT GPIOC
+#define NUM_DISPLAYS 8
+
+void initDisplay (void);
+void resetDisplay (void);
+void put_char (uint8_t);
+#endif //DISPLAY_LED_H
---
+++ Core/Src/display.c
@@ -0,0 +1,71 @@
+//
+// Created by arbin on 09.08.2021.
+//
+
+//
+// Created by arbin on 29.06.2021.
+//
+
+#include "display.h"
+#include "main.h"
+#include "gpio.h"
+#include "tim.h"
+#include "spi.h"
+#include <string.h>
+
+uint8_t buf[NUM_DISPLAYS][6];
+uint8_t cycle ;
+uint8_t workingField[NUM_DISPLAYS+1];
+
+
+
+void initDisplay (void)
+{
+    memset(buf, 0, sizeof(buf));
+    memset(workingField, 0xAA, sizeof(workingField));
+    cycle = 0;
+    resetDisplay();
+    HAL_TIM_Base_Start_IT(&htim2);
+
+}
+void resetDisplay (void)
+{
+    HAL_GPIO_WritePin(SRCLR_GPIO_Port, SRCLR_Pin, GPIO_PIN_RESET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(SRCLR_GPIO_Port, SRCLR_Pin, GPIO_PIN_SET);
+    memset(buf, 0xFF ,sizeof(buf));
+    memset(workingField, 0xFF, sizeof (workingField) -1);
+    workingField[NUM_DISPLAYS] = 0xFE;
+    HAL_SPI_Transmit(&hspi1, workingField, NUM_DISPLAYS + 1, 100);
+    HAL_GPIO_WritePin(RCLK_GPIO_Port, RCLK_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(RCLK_GPIO_Port, RCLK_Pin, GPIO_PIN_RESET);
+
+}
+void updateDisplay(void)
+{
+    HAL_SPI_Transmit(&hspi1, workingField, NUM_DISPLAYS + 1, 100);
+    HAL_GPIO_WritePin(RCLK_GPIO_Port, RCLK_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(RCLK_GPIO_Port, RCLK_Pin, GPIO_PIN_RESET);
+}
+
+
+
+
+
+uint8_t takt = 0;
+
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+
+    if (htim->Instance == TIM2)
+    {
+        if (takt == 6) takt = 0;
+        workingField[NUM_DISPLAYS] = (uint8_t) ~(1<<takt);
+        updateDisplay();
+        takt++;
+
+    }
+
+}
